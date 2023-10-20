38564b8ee5eafb29e96f43d6f49b0185e15f3c5b
flavinho.svieira <flavinho.svieira@gmail.com>
2023-10-19 23:39:15 -0300

    alter main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from core.configs import settings
 from api.v1.api import api_router
 
 
-app = FastAPI(title='Cursos API - FastAPI SQL Alchemy')
+app = FastAPI(title='Api cadastro de cursos')
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 # /api/v1/cursos
